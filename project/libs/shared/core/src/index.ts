a46fc8020a053da46265ea875051c803ff948f1f
Захаров Денис <zdv.work@yandex.ru>
2024-11-30 20:39:31 +0600

    base Entity, interface User

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -1,1 +1,4 @@
-// Ре-экспорт модулей из `./lib`
+export { Entity } from './lib/base/entity';
+
+export { User } from './lib/types/user.interface';
+export { AuthUser } from './lib/types/auth-user.interface';
