4b5a4874b16131396dd4f0bc1daeb4dff4a81ed1
Zakharov Denis <zdv.work@yandex.ru>
2025-01-07 22:46:56 +0600

    подключил ValidationPipe

--- project/apps/account/src/main.ts
+++ project/apps/account/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -21,6 +21,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('spec', app, document);
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
   const configService = app.get(ConfigService);
   const port = configService.get('application.port');
   await app.listen(port);
--- project/libs/account/authentication/src/dto/create-user.dto.ts
+++ project/libs/account/authentication/src/dto/create-user.dto.ts
@@ -1,12 +1,17 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString } from "class-validator";
+import { AuthenticationValidateMessage } from "../authentication-module/authentication.constant";
 
 export class CreateUserDto {
   @ApiProperty({ description: 'User email', example: 'example@email.com' })
+  @IsEmail({}, { message: AuthenticationValidateMessage.EmailNotValid })
   public email: string;
 
   @ApiProperty({ description: 'User name', example: 'Ivanov Ivan' })
+  @IsString()
   public name: string;
 
   @ApiProperty({ description: 'User password', example: 'Pa$$w0rD' })
+  @IsString()
   public password: string;
 }
