fc83f4677bdadf0cefa47fe9b5c5968c09e9b8eb
Zakharov Denis <zdv.work@yandex.ru>
2025-01-10 17:02:26 +0600

    clean

--- project/libs/shared/core/src/lib/types/post/const.ts
+++ project/libs/shared/core/src/lib/types/post/const.ts
@@ -1,11 +1,3 @@
-// export enum PostType {
-//   video = 'video',
-//   text = 'text',
-//   quote = 'quote',
-//   photo = 'photo',
-//   link = 'link'
-// }
-
 export const PostType = {
   video: 'video',
   text: 'text',
@@ -15,26 +7,3 @@ export const PostType = {
 } as const;
 
 export type PostType = typeof PostType[keyof typeof PostType];
-
-/*
-const UserRoles = {
-  USER: 'USER',
-  ADMIN: 'ADMIN',
-  SUPER_ADMIN: 'SUPER_ADMIN',
-} as const
-
-type AppUserRole = typeof UserRoles[keyof typeof UserRoles]
-*/
-
-/*
-export const Role: {
-  USER: 'USER'
-  ADMIN: 'ADMIN'
-} = {
-  USER: 'USER',
-  ADMIN: 'ADMIN',
-}
-
-export type Role = typeof Role[keyof typeof Role]
-*/
-
